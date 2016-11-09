aff74de53daa31f750c040acfb5de1facb5607b7
Aelanar <stabaurelien@gmail.com>
2016-11-09 14:23:53 +0100

    Add files via upload

--- src/ABR.java
+++ src/ABR.java
@@ -1,4 +1,67 @@
+package ABR;
 
 public class ABR {
 
+	private int valueNode;
+	private boolean estVide=true;
+	private ABR brancheD = null;
+	private ABR brancheG = null;
+	
+	
+	public ABR(){
+		//Arbre vide
+	}
+
+	public ABR(int valueNode, ABR brancheD, ABR brancheG){
+		this.valueNode = valueNode;
+		this.brancheD = brancheD;
+		this.brancheG = brancheG;
+		this.estVide = false;
+	}
+	
+	//Getter
+	
+	public ABR getG(){
+		return this.brancheG;
+	}
+	public ABR getD(){
+		return this.brancheD;
+	}
+	public int getRootValue(){
+		return this.valueNode;
+	}
+	
+	//Is Empty
+	
+	public boolean isEmpty(){
+		return this.estVide;
+	}
+	
+	// Nub Element
+	
+	public int nbElements(){
+		if(this.isEmpty()){
+			return 0;
+		}
+		else{
+			return 1 + this.getG().nbElements() + this.getD().nbElements();
+		}
+	}
+	
+	public void insert(int value){
+		if(this.isEmpty()){
+			this.valueNode = value;
+			this.estVide = false;
+			this.brancheD = new ABR();
+			this.brancheG = new ABR();
+			return;
+		}else{
+			if(this.getRootValue()>value){
+				this.getG().insert(value);
+			}else{
+				this.getD().insert(value);
+			}
+		}
+		
+	}
 }
--- src/testOne.java
+++ src/testOne.java
@@ -1,3 +1,4 @@
+package ABR;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -5,8 +6,34 @@ import org.junit.Test;
 public class testOne {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInitGetter() {
+		ABR a = new ABR();
+		assertTrue(a.isEmpty());
+		ABR b = new ABR(1,a,a);
+		assertFalse(b.isEmpty());
+		assertTrue(b.getD().isEmpty());
+		assertTrue(b.getG().isEmpty());
+		assertTrue(b.getRootValue()==1);
+	}
+	
+	@Test
+	public void testNbElements() {
+		ABR a = new ABR();
+		ABR b = new ABR(1,a,a);
+		ABR c = new ABR(2,b,b);
+		assertTrue(b.nbElements()==1);
+		assertTrue(c.nbElements()==3);
+		
+	}
+	@Test
+	public void testInsert(){
+		ABR a = new ABR();
+		a.insert(1);
+		a.insert(2);
+		a.insert(0);
+		assertTrue(a.nbElements() == 3);
+		assertTrue(a.getD().getRootValue()==2);
+		assertTrue(a.getG().getRootValue()==0);
 	}
 
 }
